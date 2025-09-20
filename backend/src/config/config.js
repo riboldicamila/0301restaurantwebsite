bd408e3120afec2a8519e0a35c8719976b0a0fdd
riboldicamila <cami.riboldi@gmail.com>
2025-09-20 16:13:10 +0200

    major refactor for presentation purpose

--- backend/src/config/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || 'yourSecretKey',
-    mongoURI: process.env.MONGO_URI || 'mongodb://localhost/restaurant', 
-  };
-  
--- backend/src/routes/reservation.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reservationController = require('../controllers/reservation.controller');
-
-router.post('/', reservationController.createReservation);
-router.get('/user/:userId', reservationController.getReservationsByUser);
-router.get('/', reservationController.getAllReservations);
-
-module.exports = router;
